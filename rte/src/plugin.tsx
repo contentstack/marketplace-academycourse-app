b60409976db6a67b2f5dbebed3e4e8fbb85b1a9e
JeevananthamD <jeevanantham.d@contentstack.com>
2023-08-28 18:24:18 +0530

    added rte

--- rte/src/plugin.tsx
+++ rte/src/plugin.tsx
@@ -1,45 +1,33 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import ContentstackSDK from "@contentstack/app-sdk";
-import parse from "html-react-parser";
-import editIcon from "./public/edit.svg";
-import plugin from "./public/plugin.svg";
-import localeTexts from "./common/locales/en-us/index";
-import "./index.css";
+import { Icon } from "@contentstack/venus-components";
+
+import { onClickHandler } from "./academy";
 
 export default ContentstackSDK.init().then(async (sdk) => {
-  const extensionObj = await sdk["location"];
-  const RTE = await extensionObj["RTEPlugin"];
-  if (!RTE) return;
+    const extensionObj = await sdk["location"];
+    const RTE = await extensionObj["RTEPlugin"]!;
+    console.info("Hello sdk", sdk);
+    if (!RTE) return;
 
-  const RtePlugin = RTE("RTE Plugin", () => ({
-    title: "JSON-RTE-Plugin",
-    icon: <img style={{ padding: "0 6px" }} src={editIcon} />,
-    render: (props: any) => {
-      return (
-        <div className="rte-container">
-          <div className="custom-field">
-            <div className="custom-field-container">
-              <div className="custom-field-icon">
-                <img src={plugin} alt="icon" />
-              </div>
-              <div className="app-component-content">
-                <h4>{localeTexts.RTE.title}</h4>
-                <p>{localeTexts.RTE.description}</p>
-              </div>
-            </div>
-          </div>
-          <div className="app-component-content">
-            <p>{parse(localeTexts.RTE.body)}</p>
-            <a href="/">{localeTexts.RTE.button.learnMore}</a>
-          </div>
-        </div>
-      );
-    },
-    displayOn: ["toolbar"],
-    elementType: ["block"],
-  }));
+    const Academy = RTE("academy", () => {
+      return {
+        title: "Capitalize",
+        icon: <Icon icon="Settings" />,
+      };
+    });
 
-  return {
-    RtePlugin,
-  };
+    //@ts-ignore
+    Academy.on("exec", async (rte: RTE) => {
+      try {
+        onClickHandler(rte);
+      } catch (e) {
+        console.error("Error", e);
+      }
+    });
+
+    return {
+      Academy,
+    };
 });
--- rte/webpack.common.js
+++ rte/webpack.common.js
@@ -1,58 +1,41 @@
 const path = require("path");
 const pkg = require("./package.json");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", pkg.entry + ".tsx"),
-  externals: {
-    react: "react",
-    "react-dom": "react-dom",
-    "@contentstack/venus-components": "@contentstack/venus-components",
-  },
-  mode: "production",
-  output: {
-    filename: pkg.output,
-    path: path.resolve(__dirname, "dist"),
-    libraryTarget: "system",
-  },
-  module: {
-    rules: [
-      {
+    entry: path.resolve(__dirname, "src", pkg.entry + ".tsx"),
+    externals: {
+        react: "react",
+        'react-dom': 'react-dom',
+        '@contentstack/venus-components': '@contentstack/venus-components'
+    },
+    mode: "production",
+    output: {
+        filename: pkg.output,
+        path: path.resolve(__dirname, "dist"),
+        libraryTarget: "system",
+    },
+    module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
+            {
+                rules: [
+                    {
+                        test: /\.css$/i,
+                        use: ["style-loader", "css-loader"],
+                    },
+                ],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
         ],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.svg/,
-        use: {
-          loader: "svg-url-loader",
-          options: {
-            iesafe: true,
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: "src/public", to: path.resolve(__dirname, "dist/public") },
-      ],
-    }),
-  ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
 };
